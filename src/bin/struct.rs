c469c0fce915d5f32da15bcdd11aa37e5219848c
vplechuc <vplechuc@gmail.com>
2023-07-27 19:39:25 -0300

    adding info to structs

--- src/bin/struct.rs
+++ src/bin/struct.rs
@@ -11,7 +11,59 @@ struct Rectangle {
     height: u32,
 }
 
+struct Product {
+    name: String,
+    price: f32,
+    in_stock: bool,
+}
+
+impl Product {
+    fn new(name: String, price: f32) -> Product {
+        Product {
+            name: name,
+            price: price,
+            in_stock: true,
+        }
+    }
+    fn get_default_sales_tax() -> f32 {
+        0.1
+    }
+    fn calculate_sales_tax(&self) -> f32 {
+        self.price * Product::get_default_sales_tax()
+    }
+    fn set_price(&mut self, price: f32) {
+        self.price = price;
+    }
+    fn buy(self) -> i32 {
+        let name = self.name;
+        println!("{name} was bought!");
+        123
+    }
+}
+
+/* Strutcs can be taken as the definition of object attributes en OOP
+allows to define a new type that acces by name the attribute
+
+*/
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 2,
+    }
+}
+
+fn set_email(u: User) -> User {
+    User {
+        email: String::from("contact@im.dev"),
+        ..u
+    }
+}
+
 fn main() {
+    // create a new intance
     let u1 = User {
         email: String::from("someone@example.com"),
         username: String::from("sunface"),
@@ -34,7 +86,14 @@ fn main() {
     dbg!(&rect1); // Print debug info to stderr
 
     println!("{:?}", rect1); // Print debug info to stdout
-                             ///////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////
+
+    let mut book = Product::new(String::from("Book"), 30.0);
+    let sales_tax = book.calculate_sales_tax();
+    println!("sales tax: {}", sales_tax);
+    book.set_price(1.0);
+    book.buy();
 
     #[derive(Debug)]
     struct Person {
@@ -62,11 +121,4 @@ fn main() {
         "The person's age from person struct is {}. {}",
         person.age, name
     );
-}
-
-fn set_email(u: User) -> User {
-    User {
-        email: String::from("contact@im.dev"),
-        ..u
-    }
 }
