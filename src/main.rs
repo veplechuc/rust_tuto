874887ce0f721a9ce3146a0b0a3fdb5407286b66
vplechuc <vplechuc@gmail.com>
2023-08-22 21:02:46 -0300

    adding examples for main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
-    let x = 5; // variables ARE inmutable!!!
+    let x: i55 = 5; //variables ARE inmutable!!!;
     println!("{}", x);
 
     // x= 10; error because it is inmutable
@@ -10,6 +12,18 @@ fn main() {
     let _variable: i128; // '_' prevent the compiler complains about unused variable
 
     println!("{}", x); // prints 6 and 6%
+                       //// read from console and prints the value
+    let mut input = String::new();
+    println!("Enter a value:");
+    io::stdin().read_line(&mut input).expect("Failed to read!!");
+    println!("the input -> {}", input);
+
+    // match io::stdin().read_line(&mut input) {
+    //     Ok(_) => {
+    //         println!("the input -> {}", input)
+    //     }
+    //     Err(error) => println!("error: {error}"),
+    // }
 }
 
 // MANAGING RUST VERSIONS
