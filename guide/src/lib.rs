b62babc1fba2d00f488d8b931d6396782f21ca52
vplechuc <vplechuc@gmail.com>
2023-10-23 09:55:16 -0300

    Lib definitions

--- guide/src/lib.rs
+++ guide/src/lib.rs
@@ -1,5 +1,34 @@
 #![allow(dead_code)]
 // must refer to a file
+// rust will try to find a file_mod1.rs  or a file_mod/mod.rs dir
+
+// use dir/mod.rs when we have a module with submodules
+
+// on mod.rs we define a module definition/func or whatever and otrher modules references
+
+//example inside of some_dir/mod.rs
+
+// fn some_fn(){}
+// mod otro;
+// mod otro_mas;
+
+//  inside lib.rs
+//  mod some_dir;
+// ...
+//  mod main_mod {}
+
+
+// project/
+// ├── Cargo.toml
+// ├── some_dir/
+// │   ├── mod.rs
+// │   └── otro.rs
+// │   └── otro_mas.rs
+// │
+// ├── lib.rs
+
+// use module.rs if only have a simple
+
 pub mod file_mod1;
 // this case refers to a folder an inside a mod.rs
 pub mod maths;
